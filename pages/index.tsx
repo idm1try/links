89953f2f222d952c7dbade5ab49287a984d21cdd
idm1try <dimasavchenko1706@icloud.com>
2022-10-08 23:36:14 +0500

    chore: update

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,7 @@ import {
   Button,
   useColorModeValue,
 } from '@chakra-ui/react';
-import { FaGithub, FaTelegram } from 'react-icons/fa';
-import { IoLogoTwitter } from 'react-icons/io5';
+import { IoLogoGithub, IoLogoTwitter } from 'react-icons/io5';
 
 export default function Home() {
   return (
@@ -48,54 +47,38 @@ export default function Home() {
             </Heading>
             <Text color='gray.500'>Frontend Developer</Text>
           </Stack>
-          <a href='https://t.me/idm1try'>
-            <Button
-              w='full'
-              mt={8}
-              bg={useColorModeValue('#151f21', 'gray.900')}
-              color='white'
-              rounded='md'
-              leftIcon={<FaTelegram />}
-              _hover={{
-                transform: 'translateY(-2px)',
-                boxShadow: 'lg',
-              }}
-            >
-              Telegram
-            </Button>
-          </a>
-          <a href='https://github.com/idm1try'>
-            <Button
-              w='full'
-              mt={3}
-              bg={useColorModeValue('#151f21', 'gray.900')}
-              color='white'
-              rounded='md'
-              leftIcon={<FaGithub />}
-              _hover={{
-                transform: 'translateY(-2px)',
-                boxShadow: 'lg',
-              }}
-            >
-              GitHub
-            </Button>
-          </a>
-          <a href='https://twitter.com/idm1try'>
-            <Button
-              w='full'
-              mt={3}
-              bg={useColorModeValue('#151f21', 'gray.900')}
-              color='white'
-              rounded='md'
-              leftIcon={<IoLogoTwitter />}
-              _hover={{
-                transform: 'translateY(-2px)',
-                boxShadow: 'lg',
-              }}
-            >
-              Twitter
-            </Button>
-          </a>
+          <Button
+            as='a'
+            href='https://github.com/idm1try'
+            w='full'
+            mt={8}
+            bg={useColorModeValue('#151f21', 'gray.900')}
+            color='white'
+            rounded='md'
+            leftIcon={<IoLogoGithub />}
+            _hover={{
+              transform: 'translateY(-2px)',
+              boxShadow: 'lg',
+            }}
+          >
+            GitHub
+          </Button>
+          <Button
+            as='a'
+            href='https://twitter.com/idm1try'
+            w='full'
+            mt={3}
+            bg={useColorModeValue('#151f21', 'gray.900')}
+            color='white'
+            rounded='md'
+            leftIcon={<IoLogoTwitter />}
+            _hover={{
+              transform: 'translateY(-2px)',
+              boxShadow: 'lg',
+            }}
+          >
+            Twitter
+          </Button>
         </Box>
       </Box>
     </Center>
