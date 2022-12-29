d877d1031074ee3b937f35287f01d8c273b687d9
idm1try <dimasavchenko1706@icloud.com>
2022-12-29 23:26:01 +0500

    fix: animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 const Home = () => (
   <div>
     <h1 className='mb-5 animate-in text-4xl font-bold'>idm1try</h1>
-    <p className='mb-5'>Hi! Here my links</p>
+    <p
+      className='mb-5 animate-in'
+      style={{ '--index': 1 } as React.CSSProperties}
+    >
+      Hi! Here my links
+    </p>
     <ul
       className='animated-list flex animate-in space-x-5'
-      style={{ '--index': 1 } as React.CSSProperties}
+      style={{ '--index': 2 } as React.CSSProperties}
     >
       <li className='transition-all duration-300'>
         <a className='underlined' href='https://github.com/idm1try'>
